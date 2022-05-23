5dab4da9158ea4254afe88e8e3d1db69118774cc
Kunal <coolkunalgupta123@gmail.com>
2022-05-23 22:46:18 +0530

    added procfile and gunicorn

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///top-movies-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 Bootstrap(app)
 
 class Movie(db.Model):
